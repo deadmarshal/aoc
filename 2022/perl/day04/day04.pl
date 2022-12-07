63c88ab2a18e4912c1af3ad2921d2388c36daf82
deadmarshal <adeadmarshal@gmail.com>
2022-12-07 09:52:50 +0330

    Day04 in Perl

---
+++ 2022/perl/day04/day04.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+# https://adventofcode.com/2022/day/4
+use strict;
+use warnings;
+
+die "Usage: ./day04.pl input.txt" unless @ARGV == 1
+  && $ARGV[0] =~ /\w+/;
+
+sub part1{
+  my ($file) = @_;
+  open(my $fh,'<',$file) or die "Can't open file: $!";
+  my $sum = 0;
+  while(<$fh>){
+    chomp;
+    if(/(\d+)-(\d+),(\d+)-(\d+)/){
+      $sum++ if(($1 <= $3) && ($4 <= $2) || ($3 <= $1) && ($2 <= $4));
+    }
+  }
+  close $fh;
+  $sum;
+}
+
+sub part2{
+  my ($file) = @_;
+  open(my $fh,'<',$file) or die "Can't open file: $!";
+  my $sum = 0;
+  while(<$fh>){
+    chomp;
+    if(/(\d+)-(\d+),(\d+)-(\d+)/){
+      $sum++ unless($4 < $1 || $2 < $3);
+    }
+  }
+  close $fh;
+  $sum;
+}
+
+printf "Part1: %d\n", part1($ARGV[0]);
+printf "Part2: %d\n", part2($ARGV[0]);
+
