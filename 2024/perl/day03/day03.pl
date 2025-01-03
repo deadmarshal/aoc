557a5b95dbc4b73e97e52cb1e8c169dcf64e77c7
deadmarshal <adeadmarshal@gmail.com>
2025-01-03 16:39:19 +0330

    added day3

--- 2024/perl/day03/day03.pl
+++ 2024/perl/day03/day03.pl
@@ -2,33 +2,44 @@
 # https://adventofcode.com/2024/day/3
 use strict;
 use warnings;
-use DDP;
 
 die "Usage: $0 input.txt" unless @ARGV == 1
   && $ARGV[0] =~ /\w+/;
 
-my $part1 = 0;
-my $part2 = 0;
+my @lines;
 
-# (do\\(\\))|(don't\\(\\))|(mul\\((\\d+),(\\d+)\\))
-sub solve{
+sub part1{
+  my ($sum,$i) = (0,0);
+  while($i < $#lines){
+    if($lines[$i] =~ /\d+/){$sum += $lines[$i++] * $lines[$i]}
+    $i++
+  }
+  $sum
+}
+
+sub part2{
+  my ($sum,$i,$do) = (0,0,1);
+  while($i < $#lines){
+    if($lines[$i] eq 'do()'){$do = 1}
+    if($lines[$i] eq 'don\'t()'){$do = 0}
+    if($lines[$i] =~ /\d+/){
+      $sum += $lines[$i++] * $lines[$i] if $do
+    }
+    $i++
+  }
+  $sum
+}
+
+sub process{
   my ($file) = @_;
   open(my $fh,'<',$file) or die "Can't open file: $!";
   my $str = do{local $/; <$fh>};
   close $fh;
-  my $enabled = 1;
-  foreach my $i(0..length $str){
-    if(substr($str,$i) =~ /do\(\)/){$enabled = 1}
-    if(substr($str,$i) =~ /don't\(\)/){$enabled = 0}
-    if(substr($str,$i) =~ m{mul\((\d{1,3}),(\d{1,3})\)}){
-      print $1, ' ', $2, "\n";
-      $part1 += $1 * $2;
-      if($enabled) {$part2 += $1 * $2}
-    }
-  }
+  @lines = $str =~ /(do\(\))|(don't\(\))|mul\((\d+),(\d+)\)/g;
+  @lines = grep {defined} @lines
 }
 
-solve($ARGV[0]);
-printf "Part1: %d\n",$part1;
-printf "Part2: %d\n",$part2;
+process($ARGV[0]);
+printf "Part1: %d\n",part1();
+printf "Part2: %d\n",part2();
 
