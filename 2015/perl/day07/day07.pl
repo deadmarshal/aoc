070347114deb7e67a8d69670564b23c5c012f3e8
Ali <adeadmarshal@gmail.com>
2025-09-10 17:29:17 +0330

    day7

--- 2015/perl/day07/day07.pl
+++ 2015/perl/day07/day07.pl
@@ -7,16 +7,72 @@ use File::Slurp;
 die "Usage: $0 input.txt" unless @ARGV == 1
   && $ARGV[0] =~ /\w+/;
 
-my @contents = read_file($ARGV[0]);
+chomp(my @contents = read_file($ARGV[0]));
+my (%value,%command);
 
-sub part1{
-  1
+my $EQUAL  = 1;
+my $OR     = 2;
+my $AND    = 3;
+my $LSHIFT = 4;
+my $RSHIFT = 5;
+my $NOT    = 6;
+my $MAX    = 65535;
+
+sub parse{
+  foreach(@contents){
+    my ($command,$target) = split /\s*->\s*/;
+    if($command =~ /^[0-9]+$/) {
+      $value{$target} = 0 + $command
+    } elsif($command =~ /^([a-z]+)$/) {
+      $command{$target} = [$EQUAL,$1]
+    } elsif($command =~ /NOT\s*([0-9]+|[a-z]+)/) {
+      $command{$target} = [$NOT,$1]
+    } elsif($command =~ /([0-9]+|[a-z]+)\s*(OR|AND|LSHIFT|RSHIFT)
+			 \s*([0-9]+|[a-z]+)/x) {
+      $command{$target} = [$2 eq 'AND' ? $AND :
+			   $2 eq 'OR' ? $OR :
+			   $2 eq 'LSHIFT' ? $LSHIFT :
+			   $2 eq 'RSHIFT' ? $RSHIFT : die, $1,$3]
+    } else {
+      die "Failed parsing $_"
+    }
+  }
 }
 
-sub part2{
-  1
+sub solve{
+  my %value = %{$_[0]};
+  my %command = %{$_[1]};
+  while(1) {
+    foreach my $target(keys %command) {
+      my ($command,@inputs) = @{$command{$target}};
+      next if grep{/[^0-9]/ && !exists $value{$_}} @inputs;
+      my @values = map{/[0-9]/ ? $_ : $value{$_}} @inputs;
+      if($command == $EQUAL) {
+	$value{$target} = $values[0]
+      } elsif($command == $NOT) {
+	$value{$target} = ~$values[0]
+      } elsif($command == $OR) {
+	$value{$target} = $values[0] | $values[1]
+      } elsif($command == $AND) {
+	$value{$target} = $values[0] & $values[1]
+      } elsif($command == $LSHIFT) {
+	$value{$target} = $values[0] << $values[1]
+      } elsif($command == $RSHIFT) {
+	$value{$target} = $values[0] >> $values[1]
+      } else {
+	die "Unknown command $command";
+      }
+      $value{$target} &= $MAX;
+      delete $command{$target};
+    }
+    last if exists $value{a} || !keys %command
+  }
+  $value{a}
 }
 
-printf "Part1: %d\n",part1; # 
-printf "Part2: %d\n",part2; # 
+parse();
+my $solve1 = solve(\%value,\%command);
+printf "Part1: %d\n",$solve1; # 46065
+$value{b} = $solve1;
+printf "Part2: %d\n",solve(\%value,\%command); # 14134
 
