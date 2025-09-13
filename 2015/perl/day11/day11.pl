0e3a21a2a58ffac00b63338b8e9c06d5cccdb289
Ali <adeadmarshal@gmail.com>
2025-09-13 10:17:37 +0330

    day11

---
+++ 2015/perl/day11/day11.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+# https://adventofcode.com/2015/day/11
+use strict;
+use warnings;
+use File::Slurp;
+
+die "Usage: $0 input.txt" unless @ARGV == 1
+  && $ARGV[0] =~ /\w+/;
+
+chomp(my @contents = read_file($ARGV[0]));
+
+sub part1{
+  
+  1
+}
+
+sub part2{
+  1
+}
+
+printf "Part1: %d\n",part1; # 
+printf "Part2: %d\n",part2; # 
+
